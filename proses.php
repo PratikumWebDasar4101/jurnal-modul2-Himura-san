d7030f0430c9ecf3afbb683743ea54ae32b6bccf
Zahid Musthofainal Akhyar <32158770+Himura-san@users.noreply.github.com>
2018-09-10 09:40:56 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,4 +1,65 @@
 <?php 
-//Tuliskan kode anda di bawah ini
+    $angka = $_POST['angka'];
+    $genap = array();
+    $ganjil = array();
+
+    function rekursif_piramid($i, $j){
+        if ($i > 0) {
+            if ($j > 0) {
+                echo "* ";
+                $j--;
+                rekursif_piramid($i, $j);
+            }
+            else {
+                echo "<br>";
+                $i--;
+                rekursif_piramid($i, $i);
+            }
+        }
+    }
+    rekursif_piramid($angka, $angka);
+
+// =============================================================================== //
+
+    for ($i = 1; $i <= $angka ; $i++) { 
+        if ( $i % 2 == 0) {
+            if (count($genap) >= 0 ) {
+                $index = count($genap);
+            }
+            $genap[$index] = $i;
+        }
+        else {
+            if (count($ganjil) >= 0 ) {
+                $index = count($ganjil);
+            }
+            $ganjil[$index] = $i;
+        }
+    }
+
+// =============================================================================== //
+    echo "<br>";
+// =============================================================================== //
+
+    echo "Genap : ";
+    for ($i = 0; $i < count($genap) ; $i++) { 
+        echo $genap[$i]." ";
+    }
+
+// =============================================================================== //
+    echo "<br>";
+// =============================================================================== //
+
+    echo "Ganjil : ";
+    for ($i = 0; $i < count($ganjil) ; $i++) { 
+        echo $ganjil[$i]." ";
+    }
+
+// =============================================================================== //
+    echo "<br><br>";
+// =============================================================================== //
+
+    echo "Jumlah Deret Genap : ".count($genap);
+    echo "<br>";
+    echo "Jumlah Deret Ganjil : ".count($ganjil);
 
 ?>
